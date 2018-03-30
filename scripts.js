75dec1f7236a3d694cbe3c38bb47dc3490bde892
Alejandra <alealfarom97@gmail.com>
2018-03-29 18:30:07 -0700

    3 new folder

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$.getJSON( "https://api.airtable.com/v0/appDfyDoNNK1cLLhG/Restaurants?api_key=keyud6f6RQFJhA9LP", function( data ) {
+  // console.log(data.records);
+ var items = [];
+  $.each( data.records, function( index, val ) {
+    //console.log(val.fields["Name"])
+    items.push( "<li id='" + val.id + "'>" + 
+    	val.fields["Name"] + " -" + 
+    	val.fields["Location"] + " -" + 
+    	val.fields["Type"] + " -" + 
+    	val.fields["Cuisine"] + " -" + 
+    	val.fields["Good options for"] + " -" + 
+    	val.fields["Rating"] + " -" + 
+    	val.fields["Cost"] + " -" + 
+    	val.fields["Delivery"] + "'</li>" );
+  });
+ 
+  $( "<ol/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
