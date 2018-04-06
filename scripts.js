a25d328de9230c0ac48e125bf6976cbed71fc4d0
Alejandra <alealfarom97@gmail.com>
2018-04-05 19:30:43 -0700

    change

--- scripts.js
+++ scripts.js
@@ -7,15 +7,15 @@ $.getJSON( "https://api.airtable.com/v0/appDfyDoNNK1cLLhG/Restaurants?api_key=ke
 
   val.fields["Name"] + "</br>" + 
   val.fields["Location"] + "</br>" + 
-  val.fields["Type"] + " -" + 
-  val.fields["Cuisine"] + " -" + 
-  val.fields["Good options for"] + " -" + 
-  val.fields["Rating"] + " -" + 
-  val.fields["Cost"] + " -" + 
+  val.fields["Type"] + " </br>" + 
+  val.fields["Cuisine"] + " </br>" + 
+  val.fields["Good options for"] + " </br>" + 
+  val.fields["Rating"] + " </br>" + 
+  val.fields["Cost"] + " </br>" + 
   val.fields["Delivery"] + "'</li>" );
   });
  
-  $( "<ol/>", {
+  $( "<ul/>", {
     "class": "my-new-list",
     html: items.join( "" )
   }).appendTo( "body" );
